ca5ca32ea93dd5065d9dcb29c735ecc993ead257
Madiha <websiteMj@hotmail.com>
2025-11-03 12:33:22 +0100

    Update the tailwindcss typography

---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+// postcss.config.js
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},   // Ensures Tailwind CSS processing
+    autoprefixer: {},  // Adds vendor prefixes to CSS properties for browser compatibility
+  },
+};
---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+// tailwind.config.cjs (note the .cjs extension)
+/** @type {import('tailwindcss').Config} */
+module.exports = { // Changed from export default
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        calligraphy: ['"Great Vibes"', 'cursive'],
+      },
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+};
