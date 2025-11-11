c97c3dca0cf970a0e506a7db9d4789b1a46c7312
Madiha <websiteMj@hotmail.com>
2025-11-11 12:46:53 +0100

    remove menu

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = { // Changed from export default
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
-    "node_modules/flowbite/**/*.js"
+    "node_modules/flowbite-react/**/*.js"
   ],
   theme: {
     extend: {
