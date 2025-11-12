aca1c839e402ea176ef946f37f768f58666efb64
Madiha <websiteMj@hotmail.com>
2025-11-12 09:33:42 +0100

    update logo

--- src/components/HeroComponent.jsx
+++ src/components/HeroComponent.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/FNLogo.png"; 
+import logo from "../assets/logo.svg"; // Adjust the path as necessary
 
 export default function HeroComponent() {
   return (
@@ -7,11 +7,12 @@ export default function HeroComponent() {
        <div className="relative z-10 flex items-center justify-center mb-8 space-x-4">
         <h1 className="text-6xl md:text-7xl font-extrabold tracking-wide font-montserrat">
           FORTI NEXIS
-        </h1> 
+        </h1> </div><br></br>
+        <div className="flex items-center justify-center mb-8"> 
         <img
           src={logo}
           alt="Company Logo"
-          className="h-12 md:h-16 w-auto" // Adjust height as needed, w-auto maintains aspect ratio
+          className="h-44 md:h-124 w-auto" // Adjust height as needed, w-auto maintains aspect ratio
         />
         </div>  
         <p className="text-xl md:text-2xl font-montserrat text-red-600">
